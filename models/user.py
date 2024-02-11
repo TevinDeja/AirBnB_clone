c7f5dceef24e9f84f1f4cd4432fa40cfd86460a8
TevinDeja <tevinowena@gmail.com>
2024-02-11 00:48:16 -0800

    Be pycodestyle compliant!

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define the User class"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Class for managing user objects"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
